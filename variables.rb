77b24a6bddfde15df3fafbf8d23716e9e7474c33
Hope Zouzas <hmzouzas@gmail.com>
2018-07-10 13:07:08 +0000

    Done.

---
+++ variables.rb
@@ -0,0 +1,25 @@
+dog = "fido"
+puts dog
+puts dog
+puts dog
+name = "fido"
+puts name
+puts name
+puts name
+best_friend = "ella"
+puts best_friend
+a = "25"
+puts a
+b = "36"
+puts b 
+dog = "Ralph"
+cat = "Whiskers"
+puts dog + cat
+x = 34679
+y = 566
+z = 47
+puts x * y * z
+s = 922530758
+puts x + y + z
+v = 35292
+puts s - v
